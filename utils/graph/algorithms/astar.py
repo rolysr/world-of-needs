a8a59945f5bbe513fa8a4820fca3d92856d03136
rolysr <rolsanchez@yandex.com>
2022-11-23 11:38:30 -0500

    Update astar.py

--- utils/graph/algorithms/astar.py
+++ utils/graph/algorithms/astar.py
@@ -57,23 +57,3 @@ def astar(initial_node: Node, destination_node : Node, graph: Graph, heuristic_f
                 size += 1
                 
     return distance_to_node[destination_node]
-
-
-
-# dist = dijkstra(initial_node, graph) # get minimum distance from initial node to all other nodes
-#     heuristic_dist = dijkstra(initial_node, graph, heuristic_function) # get minimum heuristic distance
-#     best_destination_agent = None # best destination agent to go
-#     best_heuristic_distance = inf
-#     minimum_distance_destination_agent = inf
-
-#     for destination in destination_agents_locations.keys():  # try  to select the best next destination to go
-#         destination_agent_location = destination_agents_locations[destination]
-#         destination_agent_heuristic_distance = heuristic_dist[destination_agent_location]
-
-#         # is destination is the one with the least distance and is not visited, then got there
-#         if best_heuristic_distance > destination_agent_heuristic_distance:
-#             best_heuristic_distance = destination_agent_heuristic_distance
-#             best_destination_agent = destination
-#             minimum_distance_destination_agent = dist[destination_agent_location] 
-
-#     return minimum_distance_destination_agent, best_destination_agent
