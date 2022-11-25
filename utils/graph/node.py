12a6659f3ba36b2a4159e3965e89ae4b67ee17e6
dmga44 <davidmga44@gmail.com>
2022-11-25 15:15:26 -0500

    nth merge commit

--- utils/graph/node.py
+++ utils/graph/node.py
@@ -3,16 +3,15 @@ class Node:
         A class representing a node in a graph
     """
 
-    def __init__(self, values, adjacents, id):
+    def __init__(self, adjacents, id):
         self.id = id # node identifier
-        self.values = values # The set of values stored by the node
         self.adjacents = adjacents # The node's adjacent nodes and an edge cost (destination_node, edge_cost)
 
     def __eq__(self, other) -> bool:
         return self.id == other.id
 
     def __str__(self) -> str:
-        response = "Node:\n id: {}\n values: {}\n adjacents:\n".format(self.id, self.values)
+        response = "Node:\n id: {}\n adjacents:\n".format(self.id)
         for adjacent in self.adjacents:
             response += " Node: id: {0}, edge length: {1}\n".format(adjacent[0].id,adjacent[1])
         return response
