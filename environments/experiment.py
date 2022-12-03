c853f4b153093d6d8e064d1fe79bec20406c4c6e
David Garcia <davidmga44@gmail.com>
2022-12-03 16:42:51 -0500

    Finished Experiment class

--- environments/experiment.py
+++ environments/experiment.py
@@ -29,10 +29,6 @@ class optimization_target(Enum):
 class Experiment:
     """
         Abstract class for running a sequence of self improving environment simulations.
-
-        Up to code:
-        - the delta function in the params if needed
-        - the AI optimization algorithm(s) 
     """
 
     def __init__(self, number_human_agents, number_destination_agents, number_of_needs,
@@ -104,18 +100,43 @@ class Experiment:
 
         return (offers_average_price, human_needs_density, store_offers_density, stores_total_budget, store_distribution, dsat_evaluator, temperature_function, price_factor, budget_factor)
 
+    def next_list(self, ini_vector, iteration):
+        """
+            Returns a random vector close to ini_vector.
+            Maintains the sum of the ini_vector components.
+            Paramater iteration might be used in further implementations.
+        """
+        ini_sum = 0
+        end_sum = 0
+        close_vector = ini_vector
+        for i in range(len(close_vector)):
+            ini_sum += close_vector[i]
+            close_vector[i] *= uniform(0.8, 1.2)
+            end_sum += close_vector[i]
+
+        for i in range(len(close_vector)):
+            close_vector[i] *= ini_sum/end_sum
+        return close_vector
+
+    def next_value(self, ini_value, iteration):
+        """
+            Returns a random value close to ini_value.
+            Paramater iteration might be used in further implementations.
+        """
+        return ini_value * uniform(0.8, 1.2)
+
     def run_SA_store_offers_density(self, dsat_evaluator, initial_store_offers_density, temperature_function, iterations):
-        # Switching optimization_target initial settings
         (offers_average_price, human_needs_density, store_offers_density, stores_total_budget, store_distribution, dsat_evaluator,
          temperature_function, price_factor, budget_factor) = self.default_settings(dsat_evaluator, temperature_function)
 
         if initial_store_offers_density != None:
             store_offers_density = initial_store_offers_density
 
-        actual = Environment(self.number_human_agents, self.number_destination_agents, self.number_of_needs,
-                             self.simulation_duration, self.gini_coef, self.mean_income, human_needs_density,
-                             offers_average_price, store_offers_density, stores_total_budget, store_distribution)
-        eval = actual.run_x_times(dsat_evaluator, 30, 10)
+        # The environment this optimization is going to use.
+        env = Environment(self.number_human_agents, self.number_destination_agents, self.number_of_needs,
+                          self.simulation_duration, self.gini_coef, self.mean_income, human_needs_density,
+                          offers_average_price, store_offers_density, stores_total_budget, store_distribution)
+        eval = env.run_x_times(dsat_evaluator, 30, 10)
 
         print("The initial state is:\n store_offers_density: {}\n with evaluation: {}".format(
             store_offers_density, eval))
@@ -126,22 +147,152 @@ class Experiment:
                 break
 
             new_store_offers_density = self.next_list(
-                store_offers_density, it_index, self.number_of_needs)
+                store_offers_density, it_index)
 
-            actual.store_offers_density = new_store_offers_density
+            env.store_offers_density = new_store_offers_density
 
-            delta_eval = eval - actual.run_x_times(dsat_evaluator, 30, 10)
+            delta_eval = eval - env.run_x_times(dsat_evaluator, 30, 10)
 
             if delta_eval > EPS:  # improve!
                 store_offers_density = new_store_offers_density
+                eval -= delta_eval
                 continue
 
             threshold = exp(-delta_eval / temperature)
             if uniform(0, 1) < threshold:
                 store_offers_density = new_store_offers_density
+                eval -= delta_eval
 
         print("The final state is:\n store_offers_density: {}\n with evaluation: {}".format(
             store_offers_density, eval))
+
+    def run_SA_store_distribution(self, dsat_evaluator, initial_store_distribution, temperature_function, iterations):
+        (offers_average_price, human_needs_density, store_offers_density, stores_total_budget, store_distribution, dsat_evaluator,
+         temperature_function, price_factor, budget_factor) = self.default_settings(dsat_evaluator, temperature_function)
+
+        if initial_store_distribution != None:
+            store_distribution = initial_store_distribution
+
+        # The environment this optimization is going to use.
+        env = Environment(self.number_human_agents, self.number_destination_agents, self.number_of_needs,
+                          self.simulation_duration, self.gini_coef, self.mean_income, human_needs_density,
+                          offers_average_price, store_offers_density, stores_total_budget, store_distribution)
+        eval = env.run_x_times(dsat_evaluator, 30, 10)
+
+        print("The initial state is:\n store_distribution: {}\n with evaluation: {}".format(
+            store_distribution, eval))
+
+        for it_index in range(iterations):
+            temperature = temperature_function(it_index)
+            if temperature < EPS:
+                break
+
+            new_store_distribution = self.next_list(
+                store_distribution, it_index)
+
+            env.store_distribution = new_store_distribution
+
+            delta_eval = eval - env.run_x_times(dsat_evaluator, 30, 10)
+
+            if delta_eval > EPS:  # improve!
+                store_offers_density = new_store_distribution
+                eval -= delta_eval
+                continue
+
+            threshold = exp(-delta_eval / temperature)
+            if uniform(0, 1) < threshold:
+                store_offers_density = new_store_distribution
+                eval -= delta_eval
+
+        print("The final state is:\n store_distribution: {}\n with evaluation: {}".format(
+            store_distribution, eval))
+
+    def run_SA_price_factor(self, dsat_evaluator, initial_price_factor, penalty_function, temperature_function, iterations):
+        (offers_average_price, human_needs_density, store_offers_density, stores_total_budget, store_distribution, dsat_evaluator,
+         temperature_function, price_factor, budget_factor) = self.default_settings(dsat_evaluator, temperature_function)
+
+        price_factor = initial_price_factor
+        base_offers_average_price = offers_average_price
+        offers_average_price = [price_factor *
+                                price for price in base_offers_average_price]
+
+        # The environment this optimization is going to use.
+        env = Environment(self.number_human_agents, self.number_destination_agents, self.number_of_needs,
+                          self.simulation_duration, self.gini_coef, self.mean_income, human_needs_density,
+                          offers_average_price, store_offers_density, stores_total_budget, store_distribution)
+        eval = penalty_function(env.run_x_times(
+            dsat_evaluator, 30, 10), price_factor)
+
+        print("The initial state is:\n price_factor: {}\n with evaluation: {}".format(
+            price_factor, eval))
+
+        for it_index in range(iterations):
+            temperature = temperature_function(it_index)
+            if temperature < EPS:
+                break
+
+            new_price_factor = self.next_value(price_factor, it_index)
+
+            env.offers_average_price = [new_price_factor *
+                                        price for price in base_offers_average_price]
+
+            delta_eval = eval - penalty_function(env.run_x_times(
+                dsat_evaluator, 30, 10), price_factor)
+
+            if delta_eval > EPS:  # improve!
+                price_factor = new_price_factor
+                eval -= delta_eval
+                continue
+
+            threshold = exp(-delta_eval / temperature)
+            if uniform(0, 1) < threshold:
+                price_factor = new_price_factor
+                eval -= delta_eval
+
+        print("The final state is:\n price_factor: {}\n with evaluation: {}".format(
+            price_factor, eval))
+
+    def run_SA_budget_factor(self, dsat_evaluator, initial_budget_factor, penalty_function, temperature_function, iterations):
+        (offers_average_price, human_needs_density, store_offers_density, stores_total_budget, store_distribution, dsat_evaluator,
+         temperature_function, price_factor, budget_factor) = self.default_settings(dsat_evaluator, temperature_function)
+
+        budget_factor = initial_budget_factor
+        base_stores_total_budget = stores_total_budget
+
+        # The environment this optimization is going to use.
+        env = Environment(self.number_human_agents, self.number_destination_agents, self.number_of_needs,
+                          self.simulation_duration, self.gini_coef, self.mean_income, human_needs_density,
+                          offers_average_price, store_offers_density, base_stores_total_budget * budget_factor, store_distribution)
+        eval = penalty_function(env.run_x_times(
+            dsat_evaluator, 30, 10), budget_factor)
+
+        print("The initial state is:\n budget_factor: {}\n with evaluation: {}".format(
+            budget_factor, eval))
+
+        for it_index in range(iterations):
+            temperature = temperature_function(it_index)
+            if temperature < EPS:
+                break
+
+            new_budget_factor = self.next_value(budget_factor, it_index)
+
+            env.stores_total_budget = new_budget_factor * base_stores_total_budget
+
+            delta_eval = eval - penalty_function(env.run_x_times(
+                dsat_evaluator, 30, 10), budget_factor)
+
+            if delta_eval > EPS:  # improve!
+                budget_factor = new_budget_factor
+                eval -= delta_eval
+                continue
+
+            threshold = exp(-delta_eval / temperature)
+            if uniform(0, 1) < threshold:
+                budget_factor = new_budget_factor
+                eval -= delta_eval
+
+        print("The final state is:\n budget_factor: {}\n with evaluation: {}".format(
+            budget_factor, eval))
 
     def run_simulated_annealing(self, dsat_evaluator, optimization_target: optimization_target,
                                 optimization_params, temperature_function, iterations: int = 100):
@@ -160,7 +311,7 @@ class Experiment:
                 these targets are self-explained, just a numeric value. penalty_function is for penalizing 
                 trivial improvements and the optimization objective is to minimize the penalty function. 
                 The penalty function should have two parameters: the evaluation of dissatisfaction and the 
-                factor actual value. 
+                factor value in that order. 
             - temperature_function: evaluates the index of the current iteration an returns a real value.
             Must converge to 0 when the argument increases.
             - iterations is an upperbound on the number of iterations of the simulated annealing algo.
