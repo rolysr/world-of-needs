b7b2d805ee868879cbd7e29f629ad9c3fc7ecee5
aXoXoR2 <102486333+aXoXoR2@users.noreply.github.com>
2022-12-03 12:07:23 -0500

    Update human_speed_generator.py

--- utils/generator/human_generators/human_speed_generator.py
+++ utils/generator/human_generators/human_speed_generator.py
@@ -1,6 +1,13 @@
+import statistics as st
+
 def generate_human_speed():
     """
         A method for generating human speed. 
         This is given in m/min
+        Find more information in https://www.researchgate.net/figure/The-walking-speed-of-pedestrians-is-distributed-normally-with-an-estimated-mean-of-134_fig4_286071735
+        Create a Normal Distribution Class instance
+        Generate a sample
     """
-    return 60
+    a=st.NormalDist(1.34, 0.26) 
+    speed = a.samples(1)
+    return speed[0]
